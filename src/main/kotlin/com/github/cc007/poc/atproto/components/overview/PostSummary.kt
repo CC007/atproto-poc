aa33ba624cb1f88472bdc41ee71dab784204bb86
CC007 <coolcat_the_best2@hotmail.com>
2026-04-12 21:07:40 +0200

    Replace count strings for svg image

--- src/main/kotlin/com/github/cc007/poc/atproto/components/overview/PostSummary.kt
+++ src/main/kotlin/com/github/cc007/poc/atproto/components/overview/PostSummary.kt
@@ -9,6 +9,10 @@ import work.socialhub.kbsky.model.com.atproto.label.LabelDefsLabel
 import work.socialhub.kbsky.model.share.RecordUnion
 
 private val BLUR_LABELS = setOf("porn", "sexual")
+private const val LIKE_ICON = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M12 21s-6.7-4.35-9.2-8.1C.8 9.95 2.1 6.5 5.45 5.7 7.5 5.2 9.25 5.95 10.4 7.4L12 9.4l1.6-2c1.15-1.45 2.9-2.2 4.95-1.7 3.35.8 4.65 4.25 2.65 7.2C18.7 16.65 12 21 12 21z"/></svg>"""
+private const val QUOTE_ICON = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M7.8 7.5c-1.9 0-3.3 1.5-3.3 3.6V16h4.7v-4H7.6c.08-.96.76-1.78 1.86-2.16L7.8 7.5z"/><path d="M16.3 7.5c-1.9 0-3.3 1.5-3.3 3.6V16h4.7v-4h-1.6c.08-.96.76-1.78 1.86-2.16L16.3 7.5z"/></svg>"""
+private const val REPOST_ICON = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M5.5 7h11"/><path d="M13.5 4.5 16 7l-2.5 2.5"/><path d="M18.5 17h-11"/><path d="M10.5 14.5 8 17l2.5 2.5"/></svg>"""
+private const val REPLY_ICON = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M5.5 6.5h13A1.5 1.5 0 0 1 20 8v6a1.5 1.5 0 0 1-1.5 1.5h-6.1L7.5 19v-3.5h-2A1.5 1.5 0 0 1 4 14V8a1.5 1.5 0 0 1 1.5-1.5z"/></svg>"""
 
 fun HtmlBlockTag.postSummary(post: FeedDefsPostView, parentPost: FeedDefsPostView?) {
     val needsBlur = needsBlur(post.labels)
@@ -43,7 +47,10 @@ private fun HtmlBlockTag.postSummary(
             embeds.forEach { embed(it, needsBlur) }
         }
         div(classes = "post-stats") {
-            +"Likes: $likeCount | Quotes: $quoteCount | Reposts: $repostCount | Replies: $replyCount"
+            statItem("Likes", LIKE_ICON, likeCount, "post-stat-icon-like")
+            statItem("Quotes", QUOTE_ICON, quoteCount, "post-stat-icon-quote")
+            statItem("Reposts", REPOST_ICON, repostCount, "post-stat-icon-repost")
+            statItem("Replies", REPLY_ICON, replyCount, "post-stat-icon-reply")
         }
         parentPost?.let {
             div(classes = "parent-post") {
@@ -53,6 +60,22 @@ private fun HtmlBlockTag.postSummary(
                 }
                 postSummary(it, null)
             }
+        }
+    }
+}
+
+private fun FlowContent.statItem(label: String, iconSvg: String, count: Int?, iconClass: String) {
+    span(classes = "post-stat-item") {
+        val statLabel = "$label: ${count ?: 0}"
+        attributes["aria-label"] = statLabel
+        attributes["title"] = statLabel
+        span(classes = "post-stat-icon $iconClass") {
+            unsafe {
+                +iconSvg
+            }
+        }
+        span(classes = "post-stat-count") {
+            +"${count ?: 0}"
         }
     }
 }
